921760c663a265afb91c8ea67a76989852fca0a1
Ryohei-A1228 <aritem515@gmail.com>
2021-08-10 15:17:54 +0900

    タイムラインの削除

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,6 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class='dropdown-item' href="{{ route('posts.index') }}">タイムライン</a>
                                     <a class='dropdown-item' href="{{ route('posts.create') }}">投稿する</a>
                                     <a class="dropdown-item" href="{{ route('bookmarks.index') }}">ブックマーク</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
